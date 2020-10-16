b0b2d555955230af0954f8f16a838c02e5ee3a0c
Yash Kanodia <yashkanodia16@gmail.com>
2020-10-16 22:47:30 +0000

    Parameter Handled

--- my-function.js
+++ my-function.js
@@ -8,7 +8,12 @@ const port = 3000
 app.get('/say', async (req,res)=>{
       await axios.get('https://k1zipe68zk.execute-api.us-east-2.amazonaws.com/test/say?keyword=' + req.query.keyword)
 .then(function (result) {
-     res.send(result.data)
+	 if (!req.query || !req.query.keyword){
+          res.status(400).send("Provide valid parameters");
+	 }
+	 else {
+     	  res.send(result.data);
+	}
    })
    .catch(function (error) {
       console.log(error);   
